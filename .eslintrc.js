11f4c513423cd48015663d514d616739d17eb7a9
Raynek <ihorzubarev@gmail.com>
2018-01-25 01:43:53 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-    "extends": "standard"
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console":0,
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
